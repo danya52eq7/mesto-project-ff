2fff983638623a56570cc4320fcdbeb76c53ad97
Danil Yakubov <danya52eq7@yandex.ru>
2025-02-06 13:50:20 +0400

    первая_попытка

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,20 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list');  
+const cardTemplate = document.querySelector('#card-template').content;
+
+
+initialCards.forEach(function (element, deleteCard) {
+  const cardElement = cardTemplate.cloneNode(true);
+  
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__image').src = element.link;
+  
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function deleteCard(evt) {
+    evt.target.parentElement.remove();
+  });
+  
+  placesList.append(cardElement);
+});
