3f1a9cc0c0e361f9529d8aecbba5434806219c54
Danil Yakubov <danya52eq7@yandex.ru>
2025-02-06 17:53:41 +0400

    вторая_попытка

--- scripts/index.js
+++ scripts/index.js
@@ -11,16 +11,24 @@
 const placesList = document.querySelector('.places__list');  
 const cardTemplate = document.querySelector('#card-template').content;
 
+function deleteCard(item) {
+  item.remove();
+}
 
-initialCards.forEach(function (element, deleteCard) {
+function createCard(element, deleteCard) {
   const cardElement = cardTemplate.cloneNode(true);
-  
+
   cardElement.querySelector('.card__title').textContent = element.name;
   cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = element.name;
   
-  cardElement.querySelector('.card__delete-button').addEventListener('click', function deleteCard(evt) {
-    evt.target.parentElement.remove();
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function(evt) {
+    deleteCard(evt.target.parentElement);
   });
-  
-  placesList.append(cardElement);
+
+  return cardElement;
+}
+
+initialCards.forEach(function (obj) {
+  placesList.append(createCard(obj, deleteCard));
 });
